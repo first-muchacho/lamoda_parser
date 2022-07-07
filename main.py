1a35d16f25d4be6f71776a3af66e2ffa2b1e5f68
SaVladimir <vova.savenko98@mail.ru>
2022-07-08 02:21:44 +0300

    update v2 parser

--- main.py
+++ main.py
@@ -2,34 +2,7 @@ import datetime
 import requests
 import csv
 import json
-from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-
-
-# def get_pages(url):
-#     user_agent = UserAgent()
-#     headers = {
-#         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-#         'User_Agent': user_agent.random 
-#     }
-
-#     response = requests.get(url=url, headers=headers)
-
-#     with open('index.html', 'w', encoding='utf-8') as file:
-#         file.write(response.text)
-
-
-# def get_json(url):
-#     user_agent = UserAgent()
-#     headers = {
-#         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-#         'User_Agent': user_agent.random 
-#     }
-
-#     response = requests.get(url=url, headers=headers)
-
-#     with open('result.json', 'w', encoding='utf-8') as file:
-#         json.dump(response.json(), file, indent=4, ensure_ascii=False)
 
 
 def get_data():
@@ -43,9 +16,9 @@ def get_data():
     response = requests.get(url='https://www.lamoda.ru/c/4153/default-women/?is_sale=1&display_locations=outlet&sitelink=topmenuW&l=11&page=1&json=1', headers=headers)
     data = response.json()
     pagination = data.get('payload').get('pagination').get('pages')
+    
     with open(f'{today}.csv', 'w', newline='', encoding='cp1251') as file:
         writer = csv.writer(file, delimiter=';')
-        
         writer.writerow(
             (
                 'Бренд',
@@ -55,36 +28,36 @@ def get_data():
                 'Процент скидки, %',
             )
         )
-
-    for page in range(1, pagination+1):
-        url = f'https://www.lamoda.ru/c/4153/default-women/?is_sale=1&display_locations=outlet&sitelink=topmenuW&l=11&page={page}&json=1'
+    
+    for page in range(1, pagination + 1):
+        url = f'https://www.lamoda.ru/c/4153/default-women/?is_sale=1&display_locations=outlet&sitelink=topmenuW&l=11&page={page}&json={page}'
         r = requests.get(url=url, headers=headers)
         data = r.json()
-        products = data.get('payload').get('products')
-        for product in products:
-            brand = product.get('brand').get('name')
-            category = product.get('name')
-            old_price =  product.get('old_price_amount')
-            new_price =  product.get('price_amount')
-            discount = product.get('discount')
-
+        payloads = data.get('payload')
+        for payload in payloads:
+            products = payloads.get('products')
+            for product in products:
+                brand = product.get('brand').get('name')
+                category = product.get('name')
+                old_price =  product.get('old_price_amount')
+                new_price =  product.get('price_amount')
+                discount = product.get('discount')
+                
+                with open(f'{today}.csv', 'a', newline='', encoding='utf-8') as file:
+                    writer = csv.writer(file, delimiter=';')
+                    writer.writerow(
+                        (
+                            brand,
+                            category,
+                            old_price,
+                            new_price,
+                            discount,
+                        )
+                    )
         print(f'{page}/{pagination}')
-    
-        with open(f'{today}.csv', 'a', newline='', encoding='cp1251') as file:
-            writer = csv.writer(file, delimiter=';')
-            writer.writerow(
-                (
-                    brand,
-                    category,
-                    old_price,
-                    new_price,
-                    discount,
-                )
-            )
+
 
 def main():
-    # get_pages(url='https://www.lamoda.ru/c/4153/default-women/?is_sale=1&display_locations=outlet&sitelink=topmenuW&l=11')
-    # get_json(url='https://www.lamoda.ru/c/4153/default-women/?is_sale=1&display_locations=outlet&sitelink=topmenuW&l=11&page=1&json=1')
     get_data()
 
 
